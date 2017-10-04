8daf464e800f167a8d1b33ca0f6c707c8d3bc452
Jakob <jakob.lindahl@yh.nackademin.se>
2017-10-04 09:25:28 +0200

    assdasd

--- IDjur.java
+++ IDjur.java
@@ -3,6 +3,7 @@ package sprint1;
 interface IDjur {
 
     String getNamn();
-
+    
+    //polymorfism då det är en metod som gör olika saker beronde på vad det är för typ av objekt
     void printMåltid();
 }
